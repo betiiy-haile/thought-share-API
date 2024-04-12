7ed5f455273a31b91e7891d08dbdd2a9f55f1569
betiiy-haile <betelhemhaile74@gmail.com>
2024-04-12 08:15:42 +0300

    :

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -52,17 +52,21 @@ export const loginUser = asyncHandler( async (req, res) => {
         })
     }
 
-    const user = await UserModel.findOne({ email })
-    if(user && (await bcrypt.compare(password, user.password))){
-        res.status(200).json({
+    const user = await UserModel.findOne({ email });
+    if (user) {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+            res.status(200).json({
                 _id: user._id,
-                name:user.name,
+                name: user.name,
                 email: user.email,
                 token: generateToken(user._id),
-        })
-    }else {
-        res.status(401)
-        throw new Error("Invalid Credentials")
+            });
+        } else {
+            res.status(401).json({ error: "Invalid password" });
+        }
+    } else {
+        res.status(401).json({ error: "User not found" });
     }
 })
 
